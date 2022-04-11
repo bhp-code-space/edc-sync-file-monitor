ffdd2027ba1afafc5b963198c16668715196154a
Coulson Kgathi <ckgathi@gmail.com>
2022-04-12 01:13:29 +0200

    fix bugs and make updates

--- edc_sync_file_monitor/views/__init__.py
+++ edc_sync_file_monitor/views/__init__.py
@@ -1,3 +1,4 @@
+from .administration_view import AdministrationView
 from .home_view import HomeView
 from .report_view import ReportView
-from .site_monitoring_starts_view import SiteMonitoringStartView
+from .site_monitoring_starts_view_mixin import SiteMonitoringStartViewMixin
